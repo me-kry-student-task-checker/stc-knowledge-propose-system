05f6627c67dc2c18c5efc7968f5032bae47ca23d
Solyom2 <tsolyom@freemail.hu>
2021-03-12 11:25:14 +0100

    Input validáció

--- flaskr/controller/calculate_controller.py
+++ flaskr/controller/calculate_controller.py
@@ -1,11 +1,8 @@
-from flask import (
-    Blueprint,
-    request
-)
+from flask import Blueprint, request, abort
 from flaskr.service import calculate_service
 
-result = None
 bp = Blueprint("calc", __name__, url_prefix="/calc")
+operation_list = ["add", "minus", "multiple", "divide"]
 
 
 @bp.route("/", methods=["POST"])
@@ -15,10 +12,14 @@ def calculate():
     numberB = content["numberB"]
     operation = content["operation"]
 
-    error = None
-    if not numberA:
-        error = "Number A is required"
-    elif not numberB:
-        error = "Number B is required"
+    print(type(numberA))
+    print(type(numberB))
+
+    if not isinstance(numberA, int) and not isinstance(numberA, float):
+        abort(400, "First argument must be a number!")
+    elif not isinstance(numberB, int) and not isinstance(numberB, float):
+        abort(400, "Second argument must be a number!")
+    elif operation not in operation_list:
+        abort(400, "Operation must be add, minus, multiple or divide!")
 
     return calculate_service.calculate(numberA, numberB, operation)
--- flaskr/service/calculate_service.py
+++ flaskr/service/calculate_service.py
@@ -1,8 +1,11 @@
-from flask import jsonify
+from flask import jsonify, abort
 
 
 def calculate(numberA, numberB, operation):
     result = None
+
+    if numberB == 0 and operation == "divide":
+        abort(400, "You can't divide with zero")
 
     if operation == "add":
         result = numberA + numberB
