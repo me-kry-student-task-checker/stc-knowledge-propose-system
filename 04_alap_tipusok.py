a15956e2ba7dc71b01ec16282d9018ffc0f2035f
szabo.janos <Johnika06>
2021-03-30 09:56:41 +0200

    Python az alapoktól -1 YOutube oktatáson keresztül próbálom megérteni, elsajátítani a programnyelvet. https://www.youtube.com/watch?v=qTNMm07S5MQ&list=PLUnQJ5ZQoqQJT_sSKYFRu8vP6vJxdo3OZ&index=2
    https://www.youtube.com/watch?v=oB9voMXyJGg&list=PLUnQJ5ZQoqQJT_sSKYFRu8vP6vJxdo3OZ&index=4

---
+++ 04_alap_tipusok.py
@@ -0,0 +1,17 @@
+#Numbers, String, Lis, Tuple, Dictionary, user defined
+
+szam=100 #number (int, float)
+
+szam2=23.2e18 #scifi number
+
+szam3= .0867j #komplex
+
+#STRING
+
+nev1="Dalma"
+nev2="kicsibetus"
+
+
+
+
+
